815c1197824d2387f2f9ff813eedfd676bafbcd9
sathish-DS24 <sathish.rss46@gmail.com>
2025-10-27 01:08:07 +0530

    Fix mobile dark background issue - disable Tailwind dark mode completely

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: false, // Disable dark mode to prevent mobile browsers from applying dark styles
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
